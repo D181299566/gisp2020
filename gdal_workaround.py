6176c4831cce62fc25d4374e404b15e1064e1f75
Mark Foley <mark@markfoley.info>
2020-02-18 11:08:43 +0000

    Latest

--- gdal_workaround.py
+++ gdal_workaround.py
@@ -5,6 +5,9 @@ libraries such as Fiona, Shapely and Pyproj depend on it. GDAL is written in C++
 and use with Python. Python and C/C++ generally play well together but, in my experience, GDAL is an exception to this.
 
 This is only a problem if you are using MS Windows. Linux and Mac should be fine.
+-----------------------------------------------------------------------------------------------------------
+
+INSTRUCTIONS FOR MS WINDOWS USERS
 
 I also recommend that you use Python 3.7 for the moment as I have tested the setup process with this version.
 In the following examples I've given Win32 and AMD64 examples. Most modern computers will use the AMD64
@@ -26,6 +29,26 @@ You should download this file to your project directory and import it as the fir
 write. This will, hopefully, ensure a relatively painless experience when doing programming for GIS.
 
 REMEMBER: THESE INSTRUCTIONS APPLY TO MS WINDOWS USERS ONLY.
+-----------------------------------------------------------------------------------------------------------
+
+INSTRUCTIONS FOR APPLE MAC AND LINUX USERS
+
+If you are using Mac or Linux the process is simpler.
+
+For MAC:
+* Make sure that you have Homebrew installed. This is Apple's package manager for stuff other than that supplied by the
+OS or from an App Store.
+* From a TERMINAL window run: brew install gdal
+
+For LINUX: Assumption is that your OS uses the APT package manager as is the case for Debian-based distributions such as
+Ubuntu.
+* From a TERMINAL window run: sudo apt-get install libgdal-dev
+
+For Both MAC and LINUX
+* Create your PyCharm project as usual.
+* From PyCharm terminal run: pip install pygdal=="$(gdal-config --version).*"
+* From PyCharm terminal run: pip install pyproj fiona shapely
+-----------------------------------------------------------------------------------------------------------
 
 Mark Foley
 January 2019, updated January 2020
@@ -35,6 +58,7 @@ import os
 import distutils
 
 if os.name == 'nt':
+    # Only runs if os is MS Windows (coded as nt)
     from distutils.sysconfig import get_python_lib
     os.environ["PATH"] += os.pathsep + get_python_lib() + "\\osgeo"
 
